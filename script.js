f146c91d91cdabea3512722aa061327af3b88e2d
Jack Ibsen <jackibsen@Jack-Ibsen-2023.teamspace.ad>
2023-11-22 15:40:43 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const keys = require("./keys.json")
+console.log(keys)
+
+
+
+
+
+async function logWeather() {
+
+    
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Halifax&appid=${keys.WEATHER_API}&units=metric`);
+    const weather = await response.json();
+    console.log(weather);
+
+    let current_temperature = weather.main.temp
+    console.log(current_temperature)
+
+
+
+
+
+  }
+  
+logWeather()
