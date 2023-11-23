1539262dcd66501987a2ac28dee30d5baaa48fab
Kaktoose <72473270+Kaktoose@users.noreply.github.com>
2023-11-23 00:13:18 +0000

    added more weather units

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ setTimeout( () =>{
     document.getElementById("title").classList.remove('fadeIn')
     document.getElementById("title").classList.add('hidden')
     slide1()
+    
 
     
 }, 1000)
@@ -22,14 +23,34 @@ setTimeout( () =>{
 
 
 async function slide1(){
+    console.log('ern')
+    document.querySelector('html').style.backgroundColor = '#2c2ec7'
     document.getElementById("slide1").classList.remove('hidden')
     document.getElementById("slide1").classList.add('slide1')
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Halifax&appid=${keys.WEATHER_API}&units=metric`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${keys.WEATHER_API}&units=metric`);
     const weather = await response.json();
     console.log(weather);
 
-    document.getElementById("current_conditions_slide1").innerText = `${weather.name}:   ${weather.main.temp}°C`
-    console.log(current_temperature)
+    document.getElementById("slide1_title").innerText = weather.name
+
+
+    document.getElementById("current_temp_slide1").innerHTML = weather.main.temp
+
+    document.getElementById("feels_like_slide1").innerHTML = `Feels Like: ${weather.main.feels_like}`
+    document.getElementById("current_condition_slide1").innerHTML = weather.weather[0].description
+    document.getElementById("high_slide1").innerHTML = `Todays high: ${weather.main.temp_max}`
+    document.getElementById("low_slide1").innerHTML = `Todays low: ${weather.main.temp_min}`
+    document.getElementById("humidity_slide1").innerHTML = `Humidity: ${weather.main.humidity}%`
+    document.getElementById("wind_speed_slide1").innerHTML = `Wind Speed: ${weather.wind.speed}km/h`
+
+
+
+
+
+
+
+
+
 
 }
 
